bcf6440353c97d16af76f4a944468c8d12148a6b
Shironim <dimasseto.id@gmail.com>
2024-02-17 02:35:42 +0700

    feat: add custom action

--- actions/actions.py
+++ actions/actions.py
@@ -1,17 +1,6 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-
 from rasa_sdk.events import (
     SlotSet,
-    FollowupAction,
     AllSlotsReset
 )
 from rasa_sdk import Action, Tracker
@@ -23,94 +12,6 @@ import time
 def clean_name(name):
     return "".join([c for c in name if c.isalpha()])
 
-class ValidateSearchProductForm(FormValidationAction):
-
-    def name(self) -> Text:
-        return "validate_search_product_form"
-        
-    @staticmethod
-    def merk_db() -> List[Text]:
-        """Database of supported cuisines."""
-
-        return [
-            "nikon",
-            "sony",
-        ]
-        
-    @staticmethod
-    def series_db() -> List[Text]:
-        """Database of supported cuisines."""
-
-        return [
-            "a6000",
-            "a6400",
-            "d500",
-            "d70",
-            "d7100",
-            "a500",
-            "tele",
-            "kit",
-            "fix",
-        ]
-        
-    @staticmethod
-    def category_db() -> List[Text]:
-        """Database of supported cuisines."""
-
-        return [
-            "kamera",
-            "lensa",
-        ]
-    def validate_merk(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate `first_name` value."""
-
-        if value.lower() in self.merk_db():
-            return {"merk": value}
-        else:
-            dispatcher.utter_message(text="Mohon maaf, Kami hanya memiliki merk Nikon dan Sony, silakan pilih merk yang tersedia")
-            # validation failed, set this slot to None, meaning the
-            # user will be asked for the slot again
-            return {"merk": None}
-    
-    def validate_category(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate `first_name` value."""
-        if value.lower() in self.category_db():
-            return {"category": value}
-        else:
-            dispatcher.utter_message(text="ADMS menyediakan kamera dan lensa, silakan pilih kategori yang tersedia")
-            # validation failed, set this slot to None, meaning the
-            # user will be asked for the slot again
-            return {"category": None}
-    
-    def validate_series(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate `first_name` value."""
-
-        if value.lower() in self.series_db():
-            return {"series": value}
-        else:
-            dispatcher.utter_message(response="utter_tanya_ulang")
-            # validation failed, set this slot to None, meaning the
-            # user will be asked for the slot again
-            return {"series": None}
-    
 class ValidateRentProductForm(FormValidationAction):
    
     def name(self) -> Text:
@@ -142,6 +43,21 @@ class ValidateRentProductForm(FormValidationAction):
             return {"user_email": None}
         return {"user_email": value}
     
+    def validate_pick_date(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        
+        pick_date = next(tracker.get_latest_entity_values('number'), None)
+        print('pick_date: ', pick_date)
+        if int(pick_date) > 31 or int(pick_date) < 1:
+            dispatcher.utter_message(text="Tanggal pengambilan tidak valid, tolong masukkan tanggal yang benar")
+            return {"pick_date": None}
+        return {"pick_date": pick_date}
+
     def validate_return_date(
         self,
         value: Text,
@@ -154,7 +70,6 @@ class ValidateRentProductForm(FormValidationAction):
         return_date = next(tracker.get_latest_entity_values('number'), None)
         num_pick_date = int(pick_date)
         num_return_date = int(return_date)
-    # Validate return_date must be greater than rent_date
         if num_pick_date > num_return_date:
             if num_return_date > 31 or num_return_date < 1:
                 dispatcher.utter_message(text="Tanggal pengembalian tidak valid, tolong masukkan tanggal yang benar")
@@ -163,27 +78,8 @@ class ValidateRentProductForm(FormValidationAction):
             return {"return_date": None}
         elif num_pick_date < num_return_date:
             return {"return_date": value}
-        
-    def validate_pick_date(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        
-        pick_date = next(tracker.get_latest_entity_values('number'), None)
-    # Validate return_date must be greater than rent_date
-        if int(pick_date) > 31 or int(pick_date) < 1:
-            dispatcher.utter_message(text="Tanggal pengambilan tidak valid, tolong masukkan tanggal yang benar")
-            return {"pick_date": None}
-        return {"pick_date": pick_date}
-    
-    # Validate month in date entity
-    # Confirm before sending email
     
 class ActionFetchSearchProduct(Action):
-    # Action from click button UI in web app
     def name(self) -> Text:
         return "action_fetch_search_product"
 
@@ -191,57 +87,95 @@ class ActionFetchSearchProduct(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # Fetch data to check availability this product
-        # then, ask follow up question, if user want to rent this product or not
-        # if yes, then store id product to slot array
         category = tracker.get_slot("category")
         merk = tracker.get_slot("merk")
         series = tracker.get_slot("series")
-        url = f"http://localhost:5000/produk/{merk}/{series}/{category}"
-        response = requests.get(url)
+        rentang_fokus = tracker.get_slot("rentang_fokus")
+
+        search = {
+            "kategori": category,
+            "merk": merk,
+            "seri": series,
+            "rentang_fokus": rentang_fokus,
+        }
+        print('action_fetch_search_product: ', search)
+        url = f"http://localhost:3306/search-produk/"
+        response = requests.post(url, json=search)
         jsonResult = response.json()
-        # print('res: ', jsonResult)
-        # print('jumlah data: ', len(jsonResult.get('data')))
-        # print('id produk: ', jsonResult.get('data')[0].get('id_produk'))
-        # print('type: ', type(jsonResult))
-        if len(jsonResult.get('data')) == 1:
-            if jsonResult.get('data')[0].get('status') == 'tersedia':
-                return [SlotSet('product_fetch_avaliable', True), SlotSet('product_fetch', jsonResult.get('data'))]
-            else:
-                return [SlotSet('product_fetch_avaliable', False)]
-        elif len(jsonResult.get('data')) == 0:
-            dispatcher.utter_message(text=f"Maaf kak, kami tidak memiliki {category} yang kamu cari")
-            dispatcher.utter_message(response="utter_bertanya_kebutuhan_lainnya")
-        else:
-            dispatcher.utter_message(text="Silakan pilih produk yang ingin disewa")
-        # return [SlotSet('product_fetch', jsonResult.get('data'))]
-        # reset slot value, every call action tanya produk
-        # return [SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None)]
-class ActionConfirmToRent(Action):
-    # Action from click button UI in web app
+        dispatcher.utter_message(json_message=jsonResult)
+        if jsonResult.get('data') == None or len(jsonResult.get('data')) == 0:
+            return [SlotSet('product_fetch_avaliable', False), SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None), SlotSet("rentang_fokus", None)]
+        elif len(jsonResult.get('data')) >= 1:
+            dispatcher.utter_message(response="utter_menawarkan_untuk_disewa")
+            return [SlotSet('product_fetch_avaliable', True), SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None), SlotSet("rentang_fokus", None)] 
+
+
+class ActionInsertToCart(Action):
     def name(self) -> Text:
-        return "action_confirm_to_rent"
+        return "action_insert_to_cart"
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(response="utter_bertanya_kebutuhan_lainnya")
-        
-        product_fetch = tracker.get_slot("product_fetch")
-        prev_product_cart = tracker.get_slot("id_product_cart")
-        if tracker.get_slot("product_fetch_avaliable") == True:
-            if tracker.get_intent_of_latest_message() == "konfirmasi_sewa":
-                if prev_product_cart == None:
-                    return [SlotSet("confirm_to_rent", True), SlotSet('id_product_cart', product_fetch[0].get('id_produk')), SlotSet("product_fetch", None), SlotSet("product_fetch_avaliable", False), SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None), FollowupAction("utter_bertanya_kebutuhan_lainnya")]
-                else:
-                    prev_product_cart.append(product_fetch[0].get('id_produk'))
+        category = tracker.get_slot("category")
+        merk = tracker.get_slot("merk")
+        series = tracker.get_slot("series")
+        rentang_fokus = tracker.get_slot("rentang_fokus")
+        search = {
+            "kategori": category,
+            "merk": merk,
+            "seri": series,
+            "rentang_fokus": rentang_fokus,
+        }
+        print('action_insert_to_cart: ', search)
 
-                    return [SlotSet("confirm_to_rent", True), SlotSet('id_product_cart', prev_product_cart), SlotSet("product_fetch", None), SlotSet("product_fetch_avaliable", False), SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None), FollowupAction("utter_bertanya_kebutuhan_lainnya")]
-                
-            elif tracker.get_intent_of_latest_message() == "deny":
-                return [SlotSet("confirm_to_rent", False), SlotSet("product_fetch", None), SlotSet("product_fetch_avaliable", False), SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None)]
+        url = f"http://localhost:3306/search-produk/"
+        response = requests.post(url, json=search)
+        jsonResult = response.json()
+        result = jsonResult.get('data')
+        prev_product_cart = tracker.get_slot("id_product_cart")
+        if len(jsonResult.get('data')) == 1:
+            dispatcher.utter_message(response="utter_produk_masuk_keranjang_belanja")
+            if prev_product_cart == None:
+                return [SlotSet('id_product_cart', result[0].get('id_produk')), SlotSet("product_fetch_avaliable", True), SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None), SlotSet("rentang_fokus", None)]
+            else:
+                prev_product_cart.append(result[0].get('id_produk'))
+                return [SlotSet('id_product_cart', prev_product_cart), SlotSet('product_fetch_avaliable', True), SlotSet("category", None), SlotSet("merk", None), SlotSet("series", None), SlotSet("rentang_fokus", None)] 
+            
         return []
+class ActionCheckCart(Action):
+    def name(self) -> Text:
+        return "action_check_cart"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        id_product_cart = tracker.get_slot("id_product_cart")
+        
+        search = {
+            "produk": id_product_cart,
+        }
+
+        url = f"http://localhost:3306/getSeveralProduct/"
+        response = requests.post(url, json=search)
+        jsonResult = response.json()
+        if id_product_cart == None:
+            dispatcher.utter_message(response="utter_keranjang_kosong")
+        else:
+            dispatcher.utter_message(text="Isi keranjang belanja anda : ")
+            dispatcher.utter_message(json_message=jsonResult)   
+            dispatcher.utter_message(text="Apakah anda mau checkout sekarang ? ")
+        
+        return []
+class ActionCheckout(Action):
+    def name(self) -> Text:
+        return "action_checkout"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(text="Baik, saya akan bantu untuk melakukan checkout")
+        return [SlotSet('want_to_checkout', True)]
+
 class ActionEndConversation(Action):
-    # Action from click button UI in web app
     def name(self) -> Text:
         return "action_selesai_percakapan"
     def run(self, dispatcher: CollectingDispatcher,
@@ -251,19 +185,17 @@ class ActionEndConversation(Action):
 
         return [AllSlotsReset()]   
 class ActionConfirmSearchAnother(Action):
-    # Action from click button UI in web app
     def name(self) -> Text:
-        return "action_confirm_search_another_product"
+        return "action_confirm_checkout"
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        if tracker.get_intent_of_latest_message() == "konfirmasi_sewa":
-            return [SlotSet('want_to_rent_another_product', True)]
-        elif tracker.get_intent_of_latest_message() == "deny":
-            return [SlotSet('want_to_rent_another_product', False)]
+        if tracker.get_intent_of_latest_message() == "konfirmasi":
+            return [SlotSet('want_to_checkout', True)]
+        elif tracker.get_intent_of_latest_message() == "menolak":
+            return [SlotSet('want_to_checkout', False)]
         return []
 class ActionSendEmailInvoice(Action):
-    # Action from click button UI in web app
     def name(self) -> Text:
         return "action_send_email_invoice"
     def run(self, dispatcher: CollectingDispatcher,
@@ -283,32 +215,11 @@ class ActionSendEmailInvoice(Action):
             "tanggal_dikembalikan": tanggal_dikembalikan,
             "produk": produk
         }
-        url = f"http://localhost:5000/send-invoice"
-        response = requests.post(url, json=data)
-        print("JSON Response ", response.json())
+        print('action_send_email_invoice: ', data)
+        url = f"http://localhost:3306/send-invoice"
+        requests.post(url, json=data)
         return [AllSlotsReset()]
 
-class ActionRespPengembalianAlat(Action):
-
-    def name(self) -> Text:
-        return "action_resp_pengembalian_alat"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        pengembalian = tracker.get_slot("pengembalian")
-        terlambat = tracker.get_slot("terlambat")
-        if pengembalian:
-            dispatcher.utter_message(text=f"entitas pengembalian :  {pengembalian}!")
-            dispatcher.utter_message(response="utter_pengembalian_alat")
-        elif terlambat:
-            dispatcher.utter_message(text=f"entitas terlambat {terlambat}!")
-            dispatcher.utter_message(response="utter_pengembalian_alat")
-        else:
-            dispatcher.utter_message(response="utter_pengembalian_alat")
-        return []
-    
 class ActionSapaan(Action):
 
     def name(self) -> Text:
@@ -328,86 +239,4 @@ class ActionSapaan(Action):
         else:
             dispatcher.utter_message(response="utter_sapaan_malam")
         
-        # dispatcher.utter_message(text=f"ini dari action sapaan {current_time}!")
         return []
-# another validation form
-# class ValidateNewsletterForm(FormValidationAction):
-#     def name(self) -> Text:
-#         return "validate_newsletter_form"
-
-#     async def required_slots(
-#             self,
-#             slots_mapped_in_domain: List[Text],
-#             dispatcher: "CollectingDispatcher",
-#             tracker: "Tracker",
-#             domain: Dict[Text, Any],
-#     ) -> Dict[Text, Any]:
-#         if not tracker.get_slot("can_ask_age"):
-#             slots_mapped_in_domain.remove("age")
-
-#         return slots_mapped_in_domain
-
-    # def validate_return_date(
-    #     self,
-    #     value: Text,
-    #     dispatcher: CollectingDispatcher,
-    #     tracker: Tracker,
-    #     domain: Dict[Text, Any],
-    # ) -> Dict[Text, Any]:
-        
-    #     pick_date = tracker.get_slot("pick_date")
-    #     bulan = tracker.get_slot("bulan")
-    #     return_date = next(tracker.get_latest_entity_values('number'), None)
-    # # Validate return_date must be greater than rent_date
-    #     if pick_date > return_date:
-    #         if return_date > 31 or return_date < 1:
-    #             dispatcher.utter_message(text="Tanggal pengembalian tidak valid, tolong masukkan tanggal yang benar")
-    #             return {"return_date": None}
-    #         dispatcher.utter_message(text="Sepertinya ada kesalahan memasukan tanggal pengembalian, tolong masukkan tanggal yang benar")
-    #         return {"return_date": None}
-    #     elif pick_date < return_date:
-    #         if bulan == "februari" and return_date > 29:
-    #             dispatcher.utter_message(text="Tanggal pengembalian tidak valid, tolong masukkan tanggal yang benar")
-    #             return {"return_date": None}
-    #         elif bulan == "februari" and return_date < 30 and return_date > 0:
-    #             return {"return_date": value}
-        
-    # def validate_pick_date(
-    #     self,
-    #     value: Text,
-    #     dispatcher: CollectingDispatcher,
-    #     tracker: Tracker,
-    #     domain: Dict[Text, Any],
-    # ) -> Dict[Text, Any]:
-        
-    #     pick_date = next(tracker.get_latest_entity_values('number'), None)
-    #     bulan = tracker.get_slot("bulan")
-    # # Validate return_date must be greater than rent_date
-    #     if pick_date > 31 or pick_date < 1:
-    #         dispatcher.utter_message(text="Tanggal pengambilan tidak valid, tolong masukkan tanggal yang benar")
-    #         return {"pick_date": None}
-    #     elif pick_date < 32 and pick_date > 0:
-    #         if bulan == "februari" and pick_date > 29:
-    #             dispatcher.utter_message(text="Tanggal pengembalian tidak valid, tolong masukkan tanggal yang benar")
-    #             return {"return_date": None}
-    #         elif bulan == "februari" and pick_date < 30 and pick_date > 0:
-    #             return {"pick_date": pick_date}
-    #         elif not bulan == "februari" and pick_date < 32 and pick_date > 0:
-    #             return {"pick_date": pick_date}
-
-    
-    # def validate_bulan(
-    #     self,
-    #     value: Text,
-    #     dispatcher: CollectingDispatcher,
-    #     tracker: Tracker,
-    #     domain: Dict[Text, Any],
-    # ) -> Dict[Text, Any]:
-        
-    #     if value.lower() in self.bulan_db():
-    #         return {"bulan": value}
-    #     else:
-    #         dispatcher.utter_message(response="utter_tanya_ulang")
-    #         # validation failed, set this slot to None, meaning the
-    #         # user will be asked for the slot again
-    #         return {"bulan": None}
